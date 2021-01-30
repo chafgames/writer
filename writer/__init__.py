a42b9472a126088a27b562934b786c3d79f8de7b
Matt Mulhern <mmulhern@proofpoint.com>
2021-01-30 11:43:57 +0000

    fix old inits
    Signed-off-by: Matt Mulhern <mmulhern@proofpoint.com>

--- writer/__init__.py
+++ writer/__init__.py
@@ -1,7 +1,5 @@
 from .main import *  # noqa: F401,F403
-from .panels import *  # noqa: F401,F403
 from .config import *  # noqa: F401,F403
-from .matics import *  # noqa: F401,F403
 from .textframe import *  # noqa: F401,F403
 from .gamecontroller import *  # noqa: F401,F403
 from .gamestate import *  # noqa: F401,F403
