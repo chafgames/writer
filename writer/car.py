9cde20d5e25ad28c9e8f10f0698449e6e5637242
Matt Mulhern <mmulhern@proofpoint.com>
2021-01-31 13:19:57 +0000

    car fixes
    Signed-off-by: Matt Mulhern <mmulhern@proofpoint.com>

--- writer/car.py
+++ writer/car.py
@@ -9,13 +9,13 @@ from asciimatics.renderers import FigletText
 from asciimatics.exceptions import NextScene
 
 from pkg_resources import resource_string
-
+from writer import constants
 
 class Car(Scene):
     def __init__(self, screen, effects=[], duration=400, clear=True, name='Car'):
         super().__init__(effects, duration, clear, name)
         self._screen = screen
-        self.car_text = resource_string('writer.art', 'car_road')
+        self.car_text = resource_string('writer.art', 'car_road').decode('utf-8')
 
 
     def intro_text(self, text, start, x, y):
@@ -31,7 +31,7 @@ class Car(Scene):
         effects = [
             Rain(self._screen, self.duration),
             self.intro_text(self.car_text, 0, x=22, y=10),
-            self.intro_text(self.story_text, 100, x=2, y=45),
+            self.intro_text(constants.car_scene_story_text, 100, x=2, y=45),
         ]
         for fx in effects:
             self.add_effect(fx)
